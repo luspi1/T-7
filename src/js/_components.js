025a22521a79ff57021899799b8a3573b7c1bc79
luspi1 <47425618+luspi1@users.noreply.github.com>
2024-02-13 17:42:50 +0300

    правки инициализации

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,4 @@
 import "./components/customSelect";
-import "./components/manageChangeableLists";
 import "./components/customDate";
 import "./components/manageModal";
 import "./components/inputMask";
